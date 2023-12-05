80f1c275f91e1a7b5156d44e67c79117c6e2a700
sgbyun <79850241+sgbyun@users.noreply.github.com>
2023-12-05 14:20:51 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,9 @@
+const input = require("fs")
+  .readFileSync(process.platform == "linux" ? "dev/stdin" : "boj/test/test.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [a, b] = input[0].split(" ").map(Number);
+console.log(a / b);
+
