ce052ab1824fb3b3e32c73c97267e312969bb7ff
sgbyun <79850241+sgbyun@users.noreply.github.com>
2023-12-05 16:13:31 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,8 @@
+const input = require("fs")
+  .readFileSync(process.platform == "linux" ? "dev/stdin" : "boj/test/test.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [a] = input[0].split(" ");
+console.log(a.charCodeAt());
