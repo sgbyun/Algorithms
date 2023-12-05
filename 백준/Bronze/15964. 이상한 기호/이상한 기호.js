e40314a4379e641a8176a6a9c8d5cb65b30603cb
sgbyun <79850241+sgbyun@users.noreply.github.com>
2023-12-05 16:45:15 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 9328 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.js
@@ -0,0 +1,8 @@
+const input = require("fs")
+  .readFileSync(process.platform == "linux" ? "dev/stdin" : "boj/test/test.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [a, b] = input[0].split(" ").map(Number);
+console.log((a + b) * (a - b));
