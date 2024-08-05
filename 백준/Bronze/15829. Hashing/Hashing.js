34a322a4a5929022235c97c14e14dc1f92b6970d
sgbyun <79850241+sgbyun@users.noreply.github.com>
2024-08-05 16:16:38 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 9576 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -7,9 +7,14 @@ const input = require("fs")
 const length = input[0].split(" ").map(Number);
 const string = input[1].split(" ");
 const r = 31;
+const M = 1234567891;
+
 let hash = 0;
+let power = 1;
+
 for (let i = 0; i < length; i++) {
   let alphabet = string[0].charCodeAt(i) - 96;
-  hash += alphabet * r ** i;
+  hash = (hash + ((alphabet * power) % M)) % M;
+  power = (power * r) % M;
 }
 console.log(hash);
